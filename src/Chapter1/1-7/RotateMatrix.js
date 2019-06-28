d01466890b50f46bc56976e8a3d051feda65935f
Sei-Yukinari <sei.yukinari@nextremer.com>
2019-06-28 14:47:23 +0900

    type change

--- src/Chapter1/1-7/RotateMatrix.js
+++ src/Chapter1/1-7/RotateMatrix.js
@@ -1,5 +1,5 @@
 /* @flow */
-export default function rotateMatrix(matrix: Array): Array {
+export default function rotateMatrix(matrix: Array<Array<number>>): Array<Array<number>> {
   const dimensions = matrix.length - 1;
   const result = [];
   matrix.forEach((column, columnIndex) => {
